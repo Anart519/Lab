d8c2d5b74f62fa05e45cf753e21a890fd25a253e
Vanaigr <65824523+VanaIgr@users.noreply.github.com>
2023-02-09 15:25:11 +0500

    add bis lab 7

---
+++ BezopInfSist/Lab7/Lab7/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Lab7 {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textTB = new System.Windows.Forms.TextBox();
+			this.offsetNUD = new System.Windows.Forms.NumericUpDown();
+			this.decrL = new System.Windows.Forms.Label();
+			this.encrL = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.offsetNUD)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.textTB, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.offsetNUD, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.decrL, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.encrL, 1, 3);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(489, 313);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(3, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(105, 18);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "текст";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(3, 108);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(105, 18);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "сдвиг";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(3, 177);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(105, 36);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "результат\r\nрасшифровки";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(3, 255);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(105, 36);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "результат\r\nшифрования";
+			// 
+			// textTB
+			// 
+			this.textTB.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textTB.Location = new System.Drawing.Point(114, 3);
+			this.textTB.Multiline = true;
+			this.textTB.Name = "textTB";
+			this.textTB.Size = new System.Drawing.Size(372, 72);
+			this.textTB.TabIndex = 4;
+			this.textTB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// offsetNUD
+			// 
+			this.offsetNUD.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.offsetNUD.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.offsetNUD.Location = new System.Drawing.Point(114, 105);
+			this.offsetNUD.Name = "offsetNUD";
+			this.offsetNUD.Size = new System.Drawing.Size(372, 24);
+			this.offsetNUD.TabIndex = 5;
+			this.offsetNUD.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// decrL
+			// 
+			this.decrL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.decrL.AutoSize = true;
+			this.decrL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.decrL.Location = new System.Drawing.Point(114, 186);
+			this.decrL.Name = "decrL";
+			this.decrL.Size = new System.Drawing.Size(372, 18);
+			this.decrL.TabIndex = 6;
+			// 
+			// encrL
+			// 
+			this.encrL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.encrL.AutoSize = true;
+			this.encrL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.encrL.Location = new System.Drawing.Point(114, 264);
+			this.encrL.Name = "encrL";
+			this.encrL.Size = new System.Drawing.Size(372, 18);
+			this.encrL.TabIndex = 7;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(489, 313);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.offsetNUD)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textTB;
+		private System.Windows.Forms.NumericUpDown offsetNUD;
+		private System.Windows.Forms.Label decrL;
+		private System.Windows.Forms.Label encrL;
+	}
+}
+
---
+++ BezopInfSist/Lab7/Lab7/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab7 {
+	public partial class Form1 : Form {
+		char[] alphabet = new[] {
+			'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 
+			'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 
+			'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 
+			'э', 'ю', 'я', 
+		};
+
+		public Form1() {
+			InitializeComponent();
+			offsetNUD.Minimum = -alphabet.Length;
+			offsetNUD.Maximum =  alphabet.Length;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e) {
+			upd();
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+			upd();
+		}
+
+		private void upd() {
+			var text = textTB.Text;
+			var offset = (int) offsetNUD.Value;
+			var alp = alphabet.Length;
+
+			var encr = new StringBuilder(text.Length);
+			var decr = new StringBuilder(text.Length);
+			foreach(var ch in text) {
+				if(char.IsWhiteSpace(ch)) {
+					encr.Append(ch);
+					decr.Append(ch);
+					continue;
+				}
+				var index = Array.IndexOf(alphabet, char.ToLower(ch));
+				if(index == -1) {
+					decrL.Text = "неправильный символ `" + ch + "` в тексте";
+					encrL.Text = "";
+					return;
+				}
+				encr.Append(alphabet[((index + offset) % alp + alp) % alp]);
+				decr.Append(alphabet[((index - offset) % alp + alp) % alp]);
+			}
+			encrL.Text = encr.ToString();
+			decrL.Text = decr.ToString();
+		}
+	}
+}
